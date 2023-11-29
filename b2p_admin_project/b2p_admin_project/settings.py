be2f40df815ab517d05c59dd5324a7285c49640c
AndrewGrayHD <65875742+AndrewGrayHD@users.noreply.github.com>
2023-11-29 12:22:40 -0800

    Update settings.py

--- b2p_admin_project/b2p_admin_project/settings.py
+++ b2p_admin_project/b2p_admin_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0i%xq(1tmibcruj-m5b1f7)emu54o3ve&b6&fu9hp+27)lb&e7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
