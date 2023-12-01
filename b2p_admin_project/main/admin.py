5eddefe06562ac701e08cf206bfa8acaf17b570e
AndrewGrayHD <jacdegracia@vxi.com.ph>
2023-11-30 21:33:25 -0800

    edited b2p admin projects

--- b2p_admin_project/main/admin.py
+++ b2p_admin_project/main/admin.py
@@ -8,12 +8,30 @@ from main.models import b2p_flex_sme_ratio_program
 from main.models import b2p_rates_program
 
 
+class  B2p_site_Admin(admin.ModelAdmin):
+    search_fields=('site',)
+
+
 class  B2p_project_nameAdmin(admin.ModelAdmin):
     search_fields=('project_2','project_1',)
 
-admin.site.register(b2p_site)
+
+class  B2p_reference_Admin(admin.ModelAdmin):
+    search_fields=('project','billing_type','effectived_date',)
+
+class  B2p_flex_agents_program_Admin(admin.ModelAdmin):
+    search_fields=('lob','site',)
+
+class  B2p_flex_sme_ratio_program_Admin(admin.ModelAdmin):
+    search_fields=('lob','site',)
+
+class  B2p_rates_program_Admin(admin.ModelAdmin):
+    search_fields=('lob','site',)
+
+
+admin.site.register(b2p_site,B2p_site_Admin)
 admin.site.register(b2p_project_name,B2p_project_nameAdmin)
-admin.site.register(b2p_reference)
-admin.site.register(b2p_flex_agents_program)
-admin.site.register(b2p_flex_sme_ratio_program)
-admin.site.register(b2p_rates_program)
+admin.site.register(b2p_reference,B2p_reference_Admin)
+admin.site.register(b2p_flex_agents_program,B2p_flex_agents_program_Admin)
+admin.site.register(b2p_flex_sme_ratio_program,B2p_flex_sme_ratio_program_Admin)
+admin.site.register(b2p_rates_program,B2p_rates_program_Admin)
