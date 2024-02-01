09605f96ded6ec77b4af3a5c35426188fbea8989
AndrewGrayHD <jacdegracia@vxi.com.ph>
2024-01-31 21:29:26 -0800

    edited b2p admin django model

--- b2p_admin_project/main/admin.py
+++ b2p_admin_project/main/admin.py
@@ -10,6 +10,26 @@ from main.models import b2p_program
 from main.models import b2p_assignment
 from main.models import b2p_program_source
 
+import csv
+from django.http import HttpResponse
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 
 class  B2p_program_Admin(admin.ModelAdmin):
@@ -30,26 +50,29 @@ class  B2p_project_nameAdmin(admin.ModelAdmin):
     list_display = ('true_program','project_1','site','effective_date','expiration_date',)
     ordering = ['-true_program__program']
 
-class  B2p_reference_Admin(admin.ModelAdmin):
+class  B2p_reference_Admin(admin.ModelAdmin,ExportCsvMixin):
     search_fields=('project__program','site__site','billing_type','effective_date','expiration_date',)
-    list_display = ('project','site','billing_type','effective_date',)
+    list_display = ('project','site','billing_type','effective_date','b2p_target','aht_cap','ob_cap','fte_cap','fte_capping','shrink','sme_shrink','flex_sme_ratio',)
     ordering = ['-effective_date']
+    actions = ["export_as_csv"]
 
-class  B2p_flex_nesting_agents_program_Admin(admin.ModelAdmin):
+class  B2p_flex_nesting_agents_program_Admin(admin.ModelAdmin,ExportCsvMixin):
     search_fields=('lob','site','true_program__program','true_site__site','assignment__assignment',)
     list_display = ('lob','site','true_program','true_site','assignment',)
     ordering = ['-true_program__program']
+    actions = ["export_as_csv"]
 
-class  B2p_flex_sme_ratio_program_Admin(admin.ModelAdmin):
+class  B2p_flex_sme_ratio_program_Admin(admin.ModelAdmin, ExportCsvMixin):
     search_fields=('lob','site','true_program__program','true_site__site','assignment__assignment',)
     list_display = ('lob','site','true_program','true_site','assignment',)
     ordering = ['-true_program__program']
+    actions = ["export_as_csv"]
 
-class  B2p_rates_program_Admin(admin.ModelAdmin):
+class  B2p_rates_program_Admin(admin.ModelAdmin, ExportCsvMixin):
     search_fields=('lob','site','true_program__program','true_site__site','assignment__assignment',)
     list_display = ('lob','site','true_program','true_site','assignment',)
     ordering = ['-true_program__program']
-
+    actions = ["export_as_csv"]
 
 class  B2p_program_sourceAdmin(admin.ModelAdmin):
     search_fields=('true_program__program','source','site__site')
